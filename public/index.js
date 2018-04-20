edd83ad2ad38aadcb0d5a18a94ed940ea6b7d60f
Sean Kimzey <sean.kimzey@gmail.com>
2018-04-20 14:43:33 -0500

    created REST routes, index page, sign up log in, log out and edit modal

--- public/index.js
+++ public/index.js
@@ -1,3 +1,44 @@
+var PostsNewPage = {
+  template: "#posts-new-page",
+  data: function() {
+    return {
+      title: "",
+      body: "",
+      image: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        title: this.title,
+        body: this.body,
+        image: this.image
+      };
+      axios
+        .post("/v1/posts", params)
+        .then(function(response) {
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
 var LoginPage = {
   template: "#login-page",
   data: function() {
@@ -30,10 +71,6 @@ var LoginPage = {
     }
   }
 };
-
-
-
-
 
 
 var SignupPage = {
@@ -70,21 +107,56 @@ var SignupPage = {
 };
 
 
-
-
-
-
-
+var PostShowPage = {
+  template: "#post-show-page",
+  data: function() {
+    return {
+      message: "Here is a page just for this post",
+      post: {}
+    };
+  },
+  created: function() {
+    axios.get("/v1/posts/" + this.$route.params.id).then(function(response) {
+      this.post = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
 
 var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      posts: [],
+      currentPost: {title: "", body: "", image: ""},
+      errors: []
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    axios.get("/v1/posts").then(function(response) {
+      console.log(response.data); 
+      this.posts = response.data;
+    }.bind(this));
+  },
+  methods: {
+    setCurrentPost: function(inputPost) {
+      this.currentPost = inputPost;
+    },
+
+    updatePost: function() {
+      var params = {
+        title: this.currentPost.title,
+        body: this.currentPost.body,
+        image: this.currentPost.image
+      };
+      axios.patch("/v1/posts/" + this.curerentPost.id, params).then(function(response) {
+        console.log('done with updating');
+      });
+
+    }
+  },
   computed: {}
 };
 
@@ -92,7 +164,11 @@ var router = new VueRouter({
   routes: [
     { path: "/", component: HomePage },
     { path: "/signup", component: SignupPage },
-    { path: "/login", component: LoginPage }
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage },
+    { path: "/posts/new", component: PostsNewPage },
+    { path: "/posts/:id", component: PostShowPage }
+
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
